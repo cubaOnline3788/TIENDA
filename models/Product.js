25f4b5a1aaacefc203a78d0b8d13c15e64898f5d
SAT3788 <46987707+cubaOnline3788@users.noreply.github.com>
2024-10-25 20:33:04 -0400

    CREANDO BACKEND

--- models/Product.js
+++ models/Product.js
@@ -1,1 +1,2 @@
 ECHO is on.
+const mongoose = require('mongoose'); const ProductSchema = new mongoose.Schema({ name: { type: String, required: true }, image: { type: String, required: true }, description: { type: String, required: true }, }); module.exports = mongoose.model('Product', ProductSchema);
